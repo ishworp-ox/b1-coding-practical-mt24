4eb2e1fc55ab0e157431586e4571b072320d87a2
ishwor <my@email.com>
2024-10-19 11:30:51 +0100

    for merge conflict testing

--- featureofnewbranch.py
+++ featureofnewbranch.py
@@ -1,3 +1,4 @@
 
 #this is all qutie something
 #this is gonna be the second commit of this graph
+#this is the second comment to see how merge conflicts work
---
+++ formergecoflicts.py
@@ -0,0 +1,1 @@
+#this file is for seeing how merge conflicts work
