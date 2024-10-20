28daa3d7aa7784e9c87919332d17b7c96582f5df
ishworp-ox <137398926+ishworp-ox@users.noreply.github.com>
2024-10-20 14:43:24 +0100

    changed the comments

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,10 +1,12 @@
 class PDController:
-    def __init__(self, kp: float, kd: float):
+    def __init__(self, kp: float, kd: float): #initalises the controller
+        #Proportional and Derivative Gains
         self.kp = kp
         self.kd = kd
+        # inital error from previous control action
         self.previous_error = 0.0
 
-    def compute_control_action(self, reference: float, output: float) -> float:
+    def compute_control_action(self, reference: float, output: float) -> float: # Computes control action using reference signal and current output
         error = reference - output
         derivative = error - self.previous_error
         control_action = self.kp * error + self.kd * derivative
