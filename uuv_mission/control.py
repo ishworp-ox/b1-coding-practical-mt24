af21e13a2818d907f5b214c8d14ac7e289950059
ishworp-ox <137398926+ishworp-ox@users.noreply.github.com>
2024-10-21 11:32:33 +0100

    updated controller

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,12 +1,12 @@
-def PDController(reference, output):
-    """
-    Brief description of the function.
+class PDController:
+    def __init__(self, K_P: float, K_D: float):
+        self.K_P = K_P
+        self.K_D = K_D
+        self.previous_error = 0
 
-    Parameters:
-    parameters (type): Description of the parameters.
-
-    Returns:
-    return_type: Description of the return value.
-    """
-    # Function implementation
-    
+    def compute_action(self, reference: float, output: float) -> float:
+        error = reference - output
+        derivative = error - self.previous_error
+        action = self.K_P * error + self.K_D * derivative
+        self.previous_error = error
+        return action
